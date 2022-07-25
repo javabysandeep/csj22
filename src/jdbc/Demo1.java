124a79d8bb9339f829a95d1564a3ff741509e528
sandeep bhingle <sandeepbhingle@gmail.com>
2022-07-25 09:41:54 +0530

    jdbc

--- src/jdbc/Demo1.java
+++ src/jdbc/Demo1.java
@@ -12,15 +12,24 @@ public class Demo1 {
         String password ="root@1234";
         String query = "select * from student";
 
+        //step 1. Register the drive with the DriverManager. It is done by loading the class
+        //Static block inside the Driver class will register the Driver
+        //Before java 1.7 we needed to load the class explicitly
+        // Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager.getConnection(url, username, password);
 
         Statement statement = connection.createStatement();
 
         ResultSet resultSet = statement.executeQuery(query);
+//        statement.execute()// DLL query
+//        statement.executeUpdate()//dml: insert, delete, update
 
         while (resultSet.next()){
             System.out.println(resultSet.getInt(1)+"\t"+
                     resultSet.getString(2));
         }
+
+        //close the connection
+        connection.close();
     }
 }
